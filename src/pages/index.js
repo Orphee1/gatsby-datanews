8489cbd1cdeb7144a73e3b8c4f2070b8db9fc4ed
HugoLattard <hugolattard@gmail.com>
2021-03-29 20:41:24 +0200

    Fixed bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,68 +4,68 @@ import { Articles, Hero, HomePictures, Layout, SEO } from "../components"
 
 export default function Home({ data }) {
   //   console.log(data)
-  const {
-    queryArticles: { nodes: articles },
-    queryPics: { nodes: pictures },
-  } = data
+  // const {
+  //   queryArticles: { nodes: articles },
+  //   queryPics: { nodes: pictures },
+  // } = data
   //   console.log(articles)
 
   return (
     <Layout>
       <SEO title="Home" />
       <Hero />
-      <Articles articles={articles} title="Derniers articles" />
-      <HomePictures pictures={pictures} />
+      {/* <Articles articles={articles} title="Derniers articles" /> */}
+      {/* <HomePictures pictures={pictures} /> */}
     </Layout>
   )
 }
-export const query = graphql`
-  {
-    queryArticles: allAirtable(
-      filter: { table: { eq: "Articles" } }
-      limit: 2
-      sort: { fields: data___date, order: DESC }
-    ) {
-      nodes {
-        id
-        data {
-          category
-          date(formatString: "MM YYYY")
-          link
-          sum
-          title
-          image {
-            localFiles {
-              childImageSharp {
-                fluid {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
-      }
-      totalCount
-    }
-    queryPics: allAirtable(
-      filter: { table: { eq: "Pics" } }
-      sort: { order: ASC, fields: data___order }
-    ) {
-      totalCount
-      nodes {
-        data {
-          location
-          image {
-            localFiles {
-              childImageSharp {
-                fluid {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   {
+//     queryArticles: allAirtable(
+//       filter: { table: { eq: "Articles" } }
+//       limit: 2
+//       sort: { fields: data___date, order: DESC }
+//     ) {
+//       nodes {
+//         id
+//         data {
+//           category
+//           date(formatString: "MM YYYY")
+//           link
+//           sum
+//           title
+//           image {
+//             localFiles {
+//               childImageSharp {
+//                 fluid {
+//                   ...GatsbyImageSharpFluid
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//       totalCount
+//     }
+//     queryPics: allAirtable(
+//       filter: { table: { eq: "Pics" } }
+//       sort: { order: ASC, fields: data___order }
+//     ) {
+//       totalCount
+//       nodes {
+//         data {
+//           location
+//           image {
+//             localFiles {
+//               childImageSharp {
+//                 fluid {
+//                   ...GatsbyImageSharpFluid
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
